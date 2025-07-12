3ef08fa03e2a01fcde60c8a08d3a2a9709dcbc36
Thanujzigma2001 <148662417+Thanujzigma2001@users.noreply.github.com>
2025-07-13 02:49:57 +0530

    Add Post

--- src/main/java/com/group/a/social_media_app/entity/Post.java
+++ src/main/java/com/group/a/social_media_app/entity/Post.java
@@ -1,4 +1,32 @@
 package com.group.a.social_media_app.entity;
 
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "posts")
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String content;
+
+    private String imageUrl;
+
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
 }
+
