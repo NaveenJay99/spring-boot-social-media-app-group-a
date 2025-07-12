8519eb3bb95af6262a5b82741a53049c299884ac
Thanujzigma2001 <148662417+Thanujzigma2001@users.noreply.github.com>
2025-07-13 03:39:41 +0530

    Add FriendRequestRepository

--- src/main/java/com/group/a/social_media_app/repository/FriendRequestRepository.java
+++ src/main/java/com/group/a/social_media_app/repository/FriendRequestRepository.java
@@ -1,4 +1,28 @@
 package com.group.a.social_media_app.repository;
 
-public interface FriendRequestRepository {
+import com.group.a.social_media_app.entity.FriendRequest;
+import com.group.a.social_media_app.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface FriendRequestRepository extends JpaRepository<FriendRequest, Long> {
+    // Find requests sent by a user
+    List<FriendRequest> findBySender(User sender);
+
+    // Find requests received by a user
+    List<FriendRequest> findByReceiver(User receiver);
+
+    // Check if a request already exists between two users
+    Optional<FriendRequest> findBySenderAndReceiver(User sender, User receiver);
+
+    // Get pending friend requests received by a user
+    List<FriendRequest> findByReceiverAndStatus(User receiver, FriendRequest.Status status);
+
+    // Get accepted friend requests for a user (either sent or received)
+    List<FriendRequest> findBySenderOrReceiverAndStatus(User sender, User receiver, FriendRequest.Status status);
+}
+
 }
