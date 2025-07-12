fcd3b4fe93df4f7cb81663dadab64cdb4131dd61
Naveen Jayathissa <naveenjay.edu@gmail.com>
2025-07-12 22:58:37 +0530

    initial commit

---
+++ src/test/java/com/group/a/social_media_app/SocialMediaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group.a.social_media_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
