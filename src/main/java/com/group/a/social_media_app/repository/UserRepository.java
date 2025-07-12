809e3ee39d20e919d1116ed080718cc2ab080d13
Naveen Jayathissa <naveenjay.edu@gmail.com>
2025-07-13 00:23:09 +0530

    remove user entity and repo

--- src/main/java/com/group/a/social_media_app/repository/UserRepository.java
+++ src/main/java/com/group/a/social_media_app/repository/UserRepository.java
@@ -1,12 +1,8 @@
 package com.group.a.social_media_app.repository;
 
 
-import com.group.a.social_media_app.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-    boolean existsByEmail(String email);
+public interface UserRepository {
+
 }
