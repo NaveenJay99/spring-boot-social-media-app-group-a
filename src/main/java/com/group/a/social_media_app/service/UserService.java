61c96b263d5bbc10d1bf91cc2a6737e90eed2c76
Naveen Jayathissa <naveenjay.edu@gmail.com>
2025-07-13 21:02:33 +0530

    third main commit

--- src/main/java/com/group/a/social_media_app/service/UserService.java
+++ src/main/java/com/group/a/social_media_app/service/UserService.java
@@ -1,4 +1,11 @@
 package com.group.a.social_media_app.service;
+
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
 @Service
 @RequiredArgsConstructor
 @Slf4j
