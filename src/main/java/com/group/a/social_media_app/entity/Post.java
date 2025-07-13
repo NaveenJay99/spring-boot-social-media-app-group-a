d93c577fb02a7834be62f150b4e4b9559644657c
Thanujzigma2001 <148662417+Thanujzigma2001@users.noreply.github.com>
2025-07-13 12:59:45 +0530

    Add Post

--- src/main/java/com/group/a/social_media_app/entity/Post.java
+++ src/main/java/com/group/a/social_media_app/entity/Post.java
@@ -1,9 +1,41 @@
 package com.group.a.social_media_app.entity;
 
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+
+
+@Entity
+@Table(name="Posts")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+
+    @Column(nullable = false,columnDefinition = "TEXT")
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;  // Assuming you have a User entity
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "like_count", nullable = false)
+    private int likeCount;
+
 
 
 }
