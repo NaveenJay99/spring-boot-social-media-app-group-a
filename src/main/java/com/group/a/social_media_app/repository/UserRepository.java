1753444c3807c90465506785495fa474eb32a609
Naveen Jayathissa <148715174+NaveenJay99@users.noreply.github.com>
2025-07-13 08:50:03 +0530

    Revert "Feature/user repository"

--- src/main/java/com/group/a/social_media_app/repository/UserRepository.java
+++ src/main/java/com/group/a/social_media_app/repository/UserRepository.java
@@ -3,24 +3,6 @@ package com.group.a.social_media_app.repository;
 
 
 
-import com.group.a.social_media_app.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+public interface UserRepository {
 
-import java.util.Optional;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    // Find a user by username
-    Optional<User> findByUsername(String username);
-
-    // Find a user by email
-    Optional<User> findByEmail(String email);
-
-    // Check if a username already exists
-    boolean existsByUsername(String username);
-
-    // Check if an email already exists
-    boolean existsByEmail(String email);
 }
