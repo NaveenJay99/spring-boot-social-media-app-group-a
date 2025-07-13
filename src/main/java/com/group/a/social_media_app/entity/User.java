91aa874b66ff9327e3aa6c0f465005ef0634346b
Naveen Jayathissa <naveenjay.edu@gmail.com>
2025-07-13 17:26:57 +0530

    create entities

--- src/main/java/com/group/a/social_media_app/entity/User.java
+++ src/main/java/com/group/a/social_media_app/entity/User.java
@@ -1,9 +1,101 @@
 package com.group.a.social_media_app.entity;
 
 
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User {
 
-}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name="is_active")
+    @Builder.Default
+    private Boolean isActive = true;
+
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Post> posts;
+
+    @OneToMany(mappedBy = "sender", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<FriendRequest> sentFriendRequests;
+
+    @OneToMany(mappedBy = "receiver", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<FriendRequest> receivedFriendRequests;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<PostLike> likes;
+
+    @ManyToMany
+    @JoinTable(
+            name = "user_friends",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "friend_id")
+    )
+
+    @Builder.Default
+    private Set<User> friends = new HashSet<>();
+
+    public String getFullName(){
+        return firstName + " " + lastName;
+    }
+
+    public void addPost(Post post) {
+        posts.add(post);
+        post.setAuthor(this);
+    }
+
+    public void removePost(Post post){
+        posts.remove(post);
+        post.setAuthor(null);
+    }
+
+    public void addFriend(User friend) {
+        this.friends.add(friend);
+        friend.getFriends().add(this);
+    }
+
+    public void removeFriend(User friend) {
+        this.friends.remove(friend);
+        friend.getFriends().remove(this);
+    }
+
+    public boolean isFriendWith(User user){
+        return this.friends.contains(user);
+    }
+
+}
--- src/main/java/com/group/a/social_media_app/repository/UserRepository.java
+++ src/main/java/com/group/a/social_media_app/repository/UserRepository.java
@@ -1,4 +1,10 @@
 package com.group.a.social_media_app.repository;
 
-public interface UserRepository {
+import com.group.a.social_media_app.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }
